dc8167916029a26343e2a72dee2cc5978da92024
Luis Leon <leon.luisgerardo775@gmail.com>
2019-03-09 18:53:28 -0600

    Update 23.c

--- 23.c
+++ 23.c
@@ -1,73 +1,115 @@
+/**
+ * @author Luis Ortega
+ *
+ *  FUCNIONA DE FORMA RECURSIVA PARA LOS OBJETOS ANIDADOS DE FORMA (A+B*(B-A*(A-B)))
+ *
+ * #NOEXAMENPARCIAL
+ */
 #include <stdio.h>
 
-char* expressionToParse = "3/4";
+char* stringOperando = "";
+void entradas();
+float evaluar();
+void salidas(float);
 
-char peek()
-{
-    return *expressionToParse;
-}
-
-char get()
-{
-    return *expressionToParse++;
-}
-
-float expression();
-
-float number()
-{
-    int result = get() - '0';
-    while (peek() >= '0' && peek() <= '9')
-    {
-        result = 10*result + get() - '0';
-    }
-    return result;
-}
-
-float factor()
-{
-    if (peek() >= '0' && peek() <= '9')
-        return number();
-    else if (peek() == '(')
-    {
-        get(); // '('
-        float result = expression();
-        get(); // ')'
-        return result;
-    }
-    else if (peek() == '-')
-    {
-        get();
-        return -factor();
-    }
-    return 0; // error
-}
-
-float term()
-{
-    float result = factor();
-    while (peek() == '*' || peek() == '/')
-        if (get() == '*')
-            result *= factor();
-        else
-            result /= factor();
-    return result;
-}
-
-float expression()
-{
-    float result = term();
-    while (peek() == '+' || peek() == '-')
-        if (get() == '+')
-            result += term();
-        else
-            result -= term();
-    return result;
-}
-
-int main()
-{
-    float resultado = expression();
-    printf("Resultado %f", resultado);
+int main(){
+    /* entradas */
+    entradas();
+    /* proceso */
+    float resultado = evaluar();
+    /* salidas */
+    salidas(resultado);
     return 0;
+}
+
+/**
+ * Entradas
+ */
+void entradas(){
+    printf("Ingresa una operacion aritmetica");
+    scanf("%s", stringOperando);
+}
+
+/**
+ * Operacion profunda
+ * @return profundidad de la anidacion || valor anidado || valor negativo en caos de serlo
+ */
+float operacion(){
+    if (*stringOperando >= '0' && *stringOperando <= '9'){
+        int operando = *stringOperando++-'0';
+        while (*stringOperando >= '0' && *stringOperando <= '9'){
+            operando= 10 * operando+ *stringOperando++ - '0';
+        }
+        return operando; //Si se puede evaluar los valores para ser evaluados
+    }
+    else if (*stringOperando == '('){
+        /*
+         * Extraemos los valores adentro de los corchetes '( A+B )'
+         */
+        stringOperando++;
+        float anidado= evaluar();
+        stringOperando++;
+        return anidado;
+    }
+    else if (*stringOperando == '-'){
+        /*
+         * Retornamos el valor negativo
+         */
+        stringOperando++;
+        return -operacion();
+    }
+    /*
+     * En caso de que ya no hayan operaciones
+     */
+    return 0;
+}
+
+/**
+ * Hace las operaciones de primer rango
+ * @return nuestra operacion de primer rango
+ */
+float primerRango(){
+    //Sacamos el char a evaluar, podria ser un numero 0..9, valores anidados entre parentesis o un numero negatico
+    float primerRango = operacion();
+    while (*stringOperando== '*' || *stringOperando == '/'){
+        /*
+         * Cuando se hace una operacion basica igual se revisa que su operacion no contenga una operacion
+         * de mayor rango
+         *
+         * EJEMPLO:
+         *
+         * 5*(2*2) || 5 / (2+3)
+         */
+        primerRango = (*stringOperando++ == '*')? primerRango * operacion() : primerRango / operacion();
+    }
+    return primerRango;
+}
+
+/**
+ * Evalua nuestra operacion aritmetica
+ * @return resultado de la operacion aritmetica
+ */
+float evaluar(){
+    /*
+     * Revisamos si nuestra operacion tiene una operacion de primer rango
+     * que serian la multiplicacion o division.
+     */
+    float resultado = primerRango();
+    //Mientras ya no tengamos operaciones aritmeticas basicas.
+    while (*stringOperando == '+' || *stringOperando == '-'){
+        /*
+         * Cuando se hace una operacion basica igual se revisa que su operacion no contenga una operacion
+         * de mayor rango
+         *
+         * EJEMPLO:
+         *
+         * 5+(2*2) || 5 - (2+3)
+         */
+        resultado = (*stringOperando++ == '+')? resultado + primerRango(): resultado - primerRango();
+    }
+    return resultado;
+}
+
+void salidas(float resultado){
+    printf("Resultado %f", resultado);
 }
