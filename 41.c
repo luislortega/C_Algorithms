3bb5495d75f3f74609abee88e7d3f165f57f175a
Luis Leon <leon.luisgerardo775@gmail.com>
2019-05-14 22:19:00 -0500

    Basic structure 41.c

--- 41.c
+++ 41.c
@@ -1,1 +1,21 @@
-Exercise.
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int id;
+    int precio;
+    int cantidad;
+} Item;
+
+typedef struct{
+    Item **item;
+    struct Node **next;
+} Node;
+
+typedef struct {
+    Node **node;
+} LinkedList;
+
+int main(){
+    LinkedList list;
+}
