be5e1d58d0d4bedf7619ab833e848a2f0c3f4324
Luis Leon <leon.luisgerardo775@gmail.com>
2019-05-14 22:35:26 -0500

    Update 41.c

--- 41.c
+++ 41.c
@@ -16,6 +16,49 @@ typedef struct {
     Node **node;
 } LinkedList;
 
+void initLinkedList(LinkedList*);
+int getOptions();
+
 int main(){
     LinkedList list;
+    initLinkedList(&list);
+    while(1){
+        switch(getOptions()){
+            case 1:
+                printf("Do something");
+                break;
+            case 2:
+                printf("Do something");
+                break;
+            case 3:
+                printf("Do something");
+                break;
+            case 4:
+                printf("Do something");
+                break;
+            case 5:
+                printf("Do something");
+                break;
+            default:
+                exit(0);
+        }
+    }
+}
+
+void initLinkedList(LinkedList *linkedList){
+    linkedList->node = NULL;
+}
+
+int getOptions(){
+    int option = 0;
+    printf("*****************CRUD MENU***************** \n");
+    printf("Welcome please type an answer... \n");
+    printf("[1] Create an Item \n");
+    printf("[2] Read an Item \n");
+    printf("[3] Update an Item \n");
+    printf("[4] Delete an Item \n");
+    printf("[5] Exit \n");
+    printf("*******************************************\n");
+    scanf("%i", &option);
+    return option;
 }
